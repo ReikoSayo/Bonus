611b4c66d546d6cecd403e1c769664d0e67e4518
ReiS <evchicrei@gmail.com>
2022-11-28 19:09:14 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int check = 100;
+        int refill = 1253;
+
+        if (refill >= 1000){
+            int bonus = refill/100;
+            check = check +refill + bonus;
+            System.out.println("Бонусов начисленно = " + bonus);
+            System.out.println("На вашем счету = " + check);
+        } else {
+            check = check +refill;
+            System.out.println("На вашем счету = " + check);
+        }
+
+    }
+}
